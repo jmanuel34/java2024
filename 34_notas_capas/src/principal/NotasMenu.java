16c412c631bbaa0c8fc4a44092002681b95a117c
Jose Manuel Mendez <=>
2024-04-02 22:36:15 +0200

    34 Empezando a separar por capas. Principal y Servicio

--- 34_notas_capas/src/principal/NotasMenu.java
+++ 34_notas_capas/src/principal/NotasMenu.java
@@ -19,7 +19,6 @@ public class NotasMenu {
 	public static void main(String[] args) {
 
 		menu();
-
 	}
 
 	public static void menu() {
@@ -28,18 +27,61 @@ public class NotasMenu {
 		String respuesta;
 		Scanner sc = new Scanner(System.in);
 		do {
-			// pedir nota, guardarla y preguntar si quiere
-			// introducir otra
-			System.out.println("Introduce nota");
-			nota = Double.parseDouble(sc.nextLine());
-			service.agregarNota(nota);
-			System.out.println("Quieres introducir otra nota?(s/n)");
+			presentarMenu();
 			respuesta = sc.nextLine();
-
-		} while (respuesta.equalsIgnoreCase("s"));
-		System.out.println("Media: " + service.calcularMedia());
-		System.out.println("Aprobados: " + totalAprobados(notas));
-		System.out.println("Nota más alta: " + notaMayor(notas));
+			switch  (respuesta) {
+				case "1":
+					introducirNota(service);
+					break;
+				case "2":
+					calcularMedia(service);
+					break;
+				case "3":
+					notasExtremas(service);
+					break;
+				case "4":
+					mostrarTodas(service);
+			}
+		} while (!respuesta.equalsIgnoreCase("5"));
+		System.out.println("BBye");
 	}
 
+	public static void presentarMenu() {
+		System.out.println("Menu de opciones:");
+		System.out.println("");
+		System.out.println("1.- Nueva nota");
+		System.out.println("2.- Calcular media:");
+		System.out.println("3.- Notas extremas");
+		System.out.println("4.- Ver todas");
+		System.out.println("5.- Salir");
+
+	}
+
+	public static void introducirNota(NotasService service) {
+		String respuesta;
+		double nota;
+		Scanner sc = new Scanner(System.in);
+		System.out.println ("Introduce una nota: ");
+		respuesta= sc.nextLine();
+		nota= Double.parseDouble(respuesta);
+		service.agregarNota(nota);	
+	}
+	
+	public static void calcularMedia(NotasService service) {
+		System.out.println("La nota media es: "+  (double)Math.round(service.calcularMedia() * 100d) / 100d);
+	}
+	
+	public static void notasExtremas(NotasService service) {
+		System.out.println("La nota mayor es: "+service.max());
+		System.out.println("La nota menor es: "+service.min());
+		
+	}
+	public static void mostrarTodas(NotasService service) {
+		double[] notas = service.devolverNotas();
+		System.out.println("Listado de todas las notas:");
+		for (double n:notas) {
+			System.out.println(n+"; ");
+		}
+	}
 }
+
