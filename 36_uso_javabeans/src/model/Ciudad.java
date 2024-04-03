241506e4a8acfdf1c5893a4410a79fd7c62d27af
manana <manana@host.docker.internal>
2024-04-03 13:36:42 +0200

    376_Java Beans patron DTO

--- 36_uso_javabeans/src/model/Ciudad.java
+++ 36_uso_javabeans/src/model/Ciudad.java
@@ -1,5 +1,54 @@
 package model;
 
 public class Ciudad {
+	private String nombre;
+	private int poblacion;
+	private double temp;
+	
+	
+	public Ciudad() {
+		super();
+	}
 
+
+	public Ciudad(String nombre, int poblacion, double temp) {
+		super();
+		this.nombre = nombre;
+		this.poblacion = poblacion;
+		this.temp = temp;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public int getPoblacion() {
+		return poblacion;
+	}
+
+
+	public void setPoblacion(int poblacion) {
+		this.poblacion = poblacion;
+	}
+
+
+	public double getTemp() {
+		return temp;
+	}
+
+
+	public void setTemp(double temp) {
+		this.temp = temp;
+	}
+	
+	
+	
+	
 }
