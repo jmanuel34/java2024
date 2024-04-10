0f3298f5dc608b75f1f7b582ee970d424ac43b62
manana <manana@host.docker.internal>
2024-04-10 13:38:25 +0200

    Final. Estudio de integracion de libreria de usuario

--- 50_empleados_fichero_lonbok/src/model/Empleado.java
+++ 50_empleados_fichero_lonbok/src/model/Empleado.java
@@ -1,6 +1,8 @@
 package model;
 
-import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
